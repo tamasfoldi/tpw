d1b8ecad8e1514854fa713700d4c6ade5b822f37
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-02-27 17:42:23 +0100

    [fix] specs

--- src/app/effects/lesson.effects.spec.ts
+++ src/app/effects/lesson.effects.spec.ts
@@ -1,10 +1,11 @@
 import { EffectsTestingModule, EffectsRunner } from '@ngrx/effects/testing';
-import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick, inject } from '@angular/core/testing';
 import { LessonEffects } from './lesson.effets';
 import { LessonService } from '../services/lesson/lesson.service';
 import { Observable } from 'rxjs/Observable';
 import * as lesson from '../actions/lesson.actions';
 import { Lesson } from '../models/lessons/lesson';
+import { LessonListElement } from '../models/lessons/lesson-list-element';
 
 describe('LessonEffects', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -20,7 +21,7 @@ describe('LessonEffects', () => {
     ]
   }));
 
-  function setup(params?: { lessonListParam: any }) {
+  function setup(params?: { lessonListParam: Observable<LessonListElement[]> }) {
     const lessonService = TestBed.get(LessonService);
     if (params) {
       lessonService.getLessonList.and.returnValue(params.lessonListParam);
@@ -32,20 +33,22 @@ describe('LessonEffects', () => {
   }
 
   describe('loadList$', () => {
-    it('should return a new lesson.LoadListSuccessAction, with the lessons, on success, after the de-bounce', fakeAsync(() => {
-      const { runner, bookEffects } = setup({ lessonListParam: Observable.of([{ id: 'test_1', title: 'Test 1', isCompleted: true }]) });
+    it('should return a new lesson.LoadListSuccessAction, with the lessons, on success, after the de-bounce', fakeAsync(
+      inject([LessonService], (lessonService: LessonService) => {
+        const { runner, bookEffects } = setup({ lessonListParam: Observable.of([{ id: 'test_1', title: 'Test 1', isCompleted: true }]) });
 
-      const expectedResult = new lesson.LoadListSuccessAction([{ id: 'test_1', title: 'Test 1', isCompleted: true }]);
-      runner.queue(new lesson.LoadListAction());
+        const expectedResult = new lesson.LoadListSuccessAction([{ id: 'test_1', title: 'Test 1', isCompleted: true }]);
+        runner.queue(new lesson.LoadListAction());
 
-      let result = null;
-      bookEffects.loadList$
-        .subscribe(_result => result = _result);
-      tick(299); // test de-bounce
-      expect(result).toBe(null);
-      tick(300);
-      expect(result).toEqual(expectedResult);
-    }));
+        let result = null;
+        bookEffects.loadList$
+          .subscribe(_result => result = _result);
+        tick(299); // test de-bounce
+        expect(result).toBe(null);
+        tick(300);
+        expect(lessonService.getLessonList).toHaveBeenCalledTimes(1);
+        expect(result).toEqual(expectedResult);
+      })));
 
   });
 });
