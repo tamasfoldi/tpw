3d8367958e0d60d0c1485ffae6eef7e74dcba36f
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-03-09 14:08:52 +0100

    [add] complete test

--- src/app/reducers/lessons.reducer.spec.ts
+++ src/app/reducers/lessons.reducer.spec.ts
@@ -1,5 +1,6 @@
 import { State, initialState, reducer } from './lessons.reducer';
 import * as lessons from '../actions/lessons.actions';
+import * as lesson from '../actions/lesson.actions';
 import { Lesson } from '../models/lessons/lesson';
 import { Action } from '@ngrx/store';
 
@@ -80,6 +81,28 @@ describe('LessonsReducer', () => {
         lessonList: []
       };
       const result = reducer(initialState, new lessons.LoadListFailAction('fail'));
+
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('COMPLETE', () => {
+    it('should set is loading false', () => {
+      const startingState: State = {
+        isLoading: false,
+        lessonList: [
+          { id: 'test_1', title: 'Test 1', isAvailable: true },
+          { id: 'test_2', title: 'Test 2', isAvailable: false }
+        ]
+      };
+      const expectedResult: State = {
+        isLoading: false,
+        lessonList: [
+          { id: 'test_1', title: 'Test 1', isAvailable: true },
+          { id: 'test_2', title: 'Test 2', isAvailable: true }
+        ]
+      };
+      const result = reducer(startingState, new lesson.CompleteAction('test_1'));
 
       expect(result).toEqual(expectedResult);
     });
