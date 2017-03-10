12b6588fd67f6acac5fc1e17822729851a351437
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-03-10 17:44:09 +0100

    [add]

--- src/app/effects/router.effects.ts
+++ src/app/effects/router.effects.ts
@@ -1,22 +1,21 @@
 // tslint:disable:member-ordering
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { Effect, Actions } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
-import * as routerActions from '../actions/router.actions';
+import * as router from '../actions/router.actions';
 import * as lessons from '../actions/lessons.actions';
-import * as lesson from '../actions/lesson.actions';
 
 @Injectable()
 export class RouterEffects {
-  constructor(private actions$: Actions, private route: ActivatedRoute, private router: Router) { }
+  constructor(private actions$: Actions, private route: ActivatedRoute) { }
 
   @Effect()
   loadSelected$: Observable<Action> = this.actions$
-    .ofType(routerActions.ActionTypes.NAVIGATION)
+    .ofType(router.ActionTypes.NAVIGATION)
     .map(action => action.payload.routerState.root.firstChild.firstChild.params)
     .select(params => params['id'])
     .filter(id => !!id)
