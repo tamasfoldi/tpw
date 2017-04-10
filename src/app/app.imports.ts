94c4cd57902a6f1f761822a35fed049796ea0f03
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-04-10 09:59:21 +0200

    [fix] linting error

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -1,4 +1,4 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
