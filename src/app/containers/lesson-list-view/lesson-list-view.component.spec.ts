0aa70b7f3990649d547b0db20e78829ad500f81a
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-04-21 10:18:05 +0200

    [add] container specs

--- src/app/containers/lesson-list-view/lesson-list-view.component.spec.ts
+++ src/app/containers/lesson-list-view/lesson-list-view.component.spec.ts
@@ -1,6 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs/observable/of';
 
 import { LessonListViewComponent } from './lesson-list-view.component';
+import { LessonListElementComponent } from '../../components/lesson-list-element/lesson-list-element.component';
 
 describe('LessonListViewComponent', () => {
   let component: LessonListViewComponent;
@@ -8,7 +15,22 @@ describe('LessonListViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LessonListViewComponent]
+      declarations: [
+        LessonListViewComponent,
+        LessonListElementComponent
+      ],
+      providers: [
+        {
+          provide: Store,
+          useClass: class {
+            select = jasmine.createSpy('select').and.returnValue(of());
+            dispatch = jasmine.createSpy('dispatch');
+          }
+        }
+      ],
+      imports: [
+        RouterTestingModule
+      ]
     })
       .compileComponents();
   }));
@@ -19,4 +41,53 @@ describe('LessonListViewComponent', () => {
     fixture.detectChanges();
   });
 
+  describe('ngOnInit', () => {
+    it('should initialize datas from Store', () => {
+      const specFixture = TestBed.createComponent(LessonListViewComponent);
+      const specComponent = specFixture.componentInstance;
+      expect(specComponent.lessons$).toBeUndefined();
+      expect(specComponent.isLoading$).toBeUndefined();
+
+      specFixture.detectChanges();
+
+      expect(specComponent.lessons$).toBeDefined();
+      expect(specComponent.isLoading$).toBeDefined();
+    });
+  });
+
+  describe('handleLessonSelect', () => {
+    it('should navigate to /lesson/:id', inject([Router], (router: Router) => {
+      spyOn(router, 'navigate');
+
+      component.handleLessonSelect('1');
+
+      expect(router.navigate).toHaveBeenCalled();
+      expect(router.navigate).toHaveBeenCalledTimes(1);
+      expect(router.navigate).toHaveBeenCalledWith(['/lesson', '1']);
+    }));
+
+    it('should be called when list element onSelect emits', () => {
+      component.lessons$ = of([{ id: '1', title: 'test', isAvailable: true }]);
+      component.isLoading$ = of(false);
+      fixture.detectChanges();
+      spyOn(component, 'handleLessonSelect');
+
+      fixture.debugElement.query(By.directive(LessonListElementComponent))
+        .injector.get(LessonListElementComponent)
+        .onSelect.emit('1');
+
+      expect(component.handleLessonSelect).toHaveBeenCalled();
+      expect(component.handleLessonSelect).toHaveBeenCalledTimes(1);
+      expect(component.handleLessonSelect).toHaveBeenCalledWith('1');
+    });
+  });
+
+  it('should display isLoading, when isLoading$ is false', () => {
+    component.isLoading$ = of(true);
+    fixture.detectChanges();
+
+    const componentInnerHTML = fixture.debugElement.nativeElement.innerHTML;
+
+    expect(componentInnerHTML).toContain('Loading...');
+  });
 });
--- src/app/containers/lesson-view/lesson-view.component.spec.ts
+++ src/app/containers/lesson-view/lesson-view.component.spec.ts
@@ -1,6 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs/observable/of';
 
 import { LessonViewComponent } from './lesson-view.component';
+import { LessonComponent } from '../../components/lesson/lesson.component';
+import { ProgressBarComponent } from '../../components/progress-bar/progress-bar.component';
+import { LessonStatisticComponent } from '../../components/lesson-statistic/lesson-statistic.component';
+import { ComputerEnemy } from '../../computer-enemy/computer-enemy';
+import { MyDomRenderer } from '../../services/renderer/my-dom-renderer';
+import * as player from '../../actions/player.actions';
 
 describe('LessonViewComponent', () => {
   let component: LessonViewComponent;
@@ -8,7 +18,29 @@ describe('LessonViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LessonViewComponent]
+      declarations: [
+        LessonViewComponent,
+        LessonComponent,
+        ProgressBarComponent,
+        LessonStatisticComponent
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: Store,
+          useClass: class {
+            select = jasmine.createSpy('select').and.returnValue(of());
+            dispatch = jasmine.createSpy('dispatch');
+          }
+        },
+        {
+          provide: ComputerEnemy,
+          useClass: class {
+            connectToRace = jasmine.createSpy('connectToRace');
+          }
+        },
+        MyDomRenderer
+      ]
     })
       .compileComponents();
   }));
@@ -17,5 +49,91 @@ describe('LessonViewComponent', () => {
     fixture = TestBed.createComponent(LessonViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  describe('ngOnInit', () => {
+    let specFixture;
+    let specComponent;
+
+    beforeEach(() => {
+      specFixture = TestBed.createComponent(LessonViewComponent);
+      specComponent = specFixture.componentInstance;
+    });
+
+    it('should initialize datas from Store', () => {
+      expect(specComponent.selectedLesson$).toBeUndefined();
+      expect(specComponent.typedText$).toBeUndefined();
+      expect(specComponent.isLessonEnded$).toBeUndefined();
+      expect(specComponent.isLessonStarted$).toBeUndefined();
+      expect(specComponent.isStarting$).toBeUndefined();
+      expect(specComponent.statistic$).toBeUndefined();
+      expect(specComponent.progress$).toBeUndefined();
+      expect(specComponent.enemiesProgress$).toBeUndefined();
+
+      specFixture.detectChanges();
+
+      expect(specComponent.selectedLesson$).toBeDefined();
+      expect(specComponent.typedText$).toBeDefined();
+      expect(specComponent.isLessonEnded$).toBeDefined();
+      expect(specComponent.isLessonStarted$).toBeDefined();
+      expect(specComponent.isStarting$).toBeDefined();
+      expect(specComponent.statistic$).toBeDefined();
+      expect(specComponent.progress$).toBeDefined();
+      expect(specComponent.enemiesProgress$).toBeDefined();
+    });
+
+    it('should connect enemy to race', inject([ComputerEnemy], (enemy: ComputerEnemy) => {
+      expect(enemy.connectToRace).toHaveBeenCalled();
+      expect(enemy.connectToRace).toHaveBeenCalledTimes(1);
+    }));
+
+    it('should connect player to race', inject([Store], (store: Store<any>) => {
+      const expectedAction = new player.NewAction('player');
+
+      expect(store.dispatch).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledTimes(1);
+      expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
+    }));
+  });
+
+  describe('handleKeyup', () => {
+    it('should navigate dispatch a new KeyAction', inject([Store], (store: Store<any>) => {
+      const expectedAction = new player.KeyAction(new KeyboardEvent('a'));
+      component.handleKeyup(new KeyboardEvent('a'));
+
+      expect(store.dispatch).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
+    }));
+
+    it('should be called when lesson component onKeyup emits', () => {
+      spyOn(component, 'handleKeyup');
+
+      fixture.debugElement.query(By.directive(LessonComponent))
+        .injector.get(LessonComponent)
+        .onKeyup.emit(new KeyboardEvent('a'));
+
+      expect(component.handleKeyup).toHaveBeenCalled();
+      expect(component.handleKeyup).toHaveBeenCalledTimes(1);
+      expect(component.handleKeyup).toHaveBeenCalledWith(new KeyboardEvent('a'));
+    });
+  });
+
+  describe('handleReady', () => {
+    it('should navigate dispatch a new ReadyAction', inject([Store], (store: Store<any>) => {
+      const expectedAction = new player.ReadyAction('player');
+      component.handleReady();
+
+      expect(store.dispatch).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
+    }));
+
+    it('should be called when button clicked', () => {
+      spyOn(component, 'handleReady');
+
+      fixture.debugElement.nativeElement.querySelector('button').click();
+
+      expect(component.handleReady).toHaveBeenCalled();
+      expect(component.handleReady).toHaveBeenCalledTimes(1);
+    });
   });
 });
