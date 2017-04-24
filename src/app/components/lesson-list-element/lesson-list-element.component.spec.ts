a1837fddb8deb74fd664b320b612a69d7225fc86
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-04-24 13:02:08 +0200

    [add] lesson list element specs

--- src/app/components/lesson-list-element/lesson-list-element.component.spec.ts
+++ src/app/components/lesson-list-element/lesson-list-element.component.spec.ts
@@ -18,4 +18,32 @@ describe('WaypointComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  it('should display the title', () => {
+    component.lesson = { id: '0', isAvailable: false, title: 'test' };
+    fixture.detectChanges();
+    expect(fixture.debugElement.nativeElement.innerHTML).toContain('test');
+  });
+
+  it(`should add available class if lesson's isAvailable is true`, () => {
+    component.lesson = { id: '0', isAvailable: false, title: 'test' };
+    fixture.detectChanges();
+    expect(fixture.debugElement.nativeElement.querySelector('.available')).toBeNull();
+
+    component.lesson = { id: '0', isAvailable: true, title: 'test' };
+    fixture.detectChanges();
+    expect(fixture.debugElement.nativeElement.querySelector('.available')).not.toBeNull();
+  });
+
+  it('should emit the id through onSelect', () => {
+    spyOn(component.onSelect, 'emit');
+    component.lesson = { id: '0', isAvailable: false, title: 'test' };
+    fixture.detectChanges();
+
+    fixture.debugElement.nativeElement.querySelector('div').click();
+
+    expect(component.onSelect.emit).toHaveBeenCalled();
+    expect(component.onSelect.emit).toHaveBeenCalledTimes(1);
+    expect(component.onSelect.emit).toHaveBeenCalledWith('0');
+  });
 });
