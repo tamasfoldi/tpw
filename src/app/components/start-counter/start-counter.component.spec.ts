af4c91764d51c18492b315a61d12be95a24a3f50
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-05-08 15:14:36 +0200

    [add] startCounting spec

--- src/app/components/start-counter/start-counter.component.spec.ts
+++ src/app/components/start-counter/start-counter.component.spec.ts
@@ -1,6 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs/observable/of';
 
 import { StartCounterComponent } from './start-counter.component';
+import * as lesson from '../../actions/lesson.actions';
 
 describe('StartCounterComponent', () => {
   let component: StartCounterComponent;
@@ -8,7 +11,16 @@ describe('StartCounterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [StartCounterComponent]
+      declarations: [StartCounterComponent],
+      providers: [
+        {
+          provide: Store,
+          useClass: class {
+            select = jasmine.createSpy('select').and.returnValue(of(true));
+            dispatch = jasmine.createSpy('dispatch');
+          }
+        },
+      ]
     })
       .compileComponents();
   }));
@@ -17,6 +29,15 @@ describe('StartCounterComponent', () => {
     fixture = TestBed.createComponent(StartCounterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  describe('startCounting', () => {
+    it('should call store dispatch 4 times and once with StartAction', fakeAsync(inject([Store], (store: Store<any>) => {
+      component.startCounting();
+      tick(3000);
+      expect(store.dispatch).toHaveBeenCalledTimes(4);
+      expect(store.dispatch).toHaveBeenCalledWith(new lesson.StartAction());
+    })));
   });
 
 });
