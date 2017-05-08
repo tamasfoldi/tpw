41fa100644375914561eb2238a2aaf053a463eb3
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-05-08 12:26:10 +0200

    [add] lesson component spec

--- src/app/components/lesson/lesson.component.spec.ts
+++ src/app/components/lesson/lesson.component.spec.ts
@@ -1,6 +1,8 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LessonComponent } from './lesson.component';
+import { MyDomRenderer } from '../../services/renderer/my-dom-renderer';
 
 describe('LessonComponent', () => {
   let component: LessonComponent;
@@ -8,7 +10,11 @@ describe('LessonComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LessonComponent]
+      declarations: [LessonComponent],
+      providers: [
+        MyDomRenderer
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents();
   }));
@@ -17,5 +23,17 @@ describe('LessonComponent', () => {
     fixture = TestBed.createComponent(LessonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  describe('handleKeyup', () => {
+    it('should emit the pressed via onKeyup', () => {
+      spyOn(component.onKeyup, 'emit');
+      const keyboardEvent = new KeyboardEvent('t');
+      component.handleKeyup(keyboardEvent);
+
+      expect(component.onKeyup.emit).toHaveBeenCalled();
+      expect(component.onKeyup.emit).toHaveBeenCalledTimes(1);
+      expect(component.onKeyup.emit).toHaveBeenCalledWith(keyboardEvent);
+    });
   });
 });
